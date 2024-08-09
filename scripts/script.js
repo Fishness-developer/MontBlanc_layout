523f31d227113ff1691c4fefe1fa231d45510fce
Fishness-developer <webmechanik@gmail.com>
2024-08-10 02:43:24 +0300

    1

--- scripts/script.js
+++ scripts/script.js
@@ -18,5 +18,44 @@
 			setCookie('id', id, 7); */
 
 
+/*let counter = 1;
+
+const buttonIncrease = document.querySelectorAll(".amount [data-amount]")
+console.log(buttonIncrease[0])
+const inputAmount = document.querySelectorAll(".amount-input");
+	buttonIncrease.forEach((button)=>{
+		button.addEventListener("click", (e)=>{
+			if(e.target==buttonIncrease[0] && counter>1){
+			    counter --
+			}else if(button==buttonIncrease[1]){
+			    counter ++
+			}
+			inputAmount.forEach((item)=>{
+				item.value =counter; 
+			})
+			
+			
+		})
+	})*/
+let counter = 1;
+
+const buttonIncrease = document.querySelectorAll(".amount [data-amount]")
+const inputAmount = document.querySelectorAll(".amount-input");
+const cart = document.querySelectorAll('[data-cart]')	 
+const cartItem = document.querySelectorAll('[data-product-name]') 
+
+	cartItem.forEach((item)=>{
+		item.addEventListener('click', (e)=>{
+			cart.forEach((item)=>{
+				if(e.target == item){
+					let name = e.currentTarget.getAttribute('data-product-name')
+					console.log(name)
+			   }
+			})
+			
+		})
+	})
+
  
-		
+
+	
