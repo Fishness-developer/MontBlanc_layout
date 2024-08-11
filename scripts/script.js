c1a18ccf3d3e7f42bcca653a0fb59122bd8cd10d
Fishness-developer <webmechanik@gmail.com>
2024-08-12 00:00:41 +0300

    1

--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,53 @@
-// Set Cookies
-/* function setCookie(name, value, days) {
-			let expires = "";
-			if (days) {
-				const date = new Date();
-				date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-				expires = "; expires=" + date.toUTCString();
-			}
-			console.log(document.cookie =name + "=" +  value + expires);
-			 
-		}
-			//const urlParams = new URL(window.location.href);
-			const urlParams = new URL('http://site.com?token=1798805276176326656-OopZ0K9glnaLEdOj34ID4xwDQmTj6i&id=1798805276176326656');
-			const token=urlParams.searchParams.get('token');
-			const id = urlParams.searchParams.get('id');
-		
-			setCookie("token", token, 7);  
-			setCookie('id', id, 7); */
-
-
-/*let counter = 1;
-
-const buttonIncrease = document.querySelectorAll(".amount [data-amount]")
-console.log(buttonIncrease[0])
-const inputAmount = document.querySelectorAll(".amount-input");
-	buttonIncrease.forEach((button)=>{
-		button.addEventListener("click", (e)=>{
-			if(e.target==buttonIncrease[0] && counter>1){
+const amount = document.querySelectorAll(".amount")
+	amount.forEach((button)=>{
+		let counter = 1;
+		button.addEventListener("click", (e)=>{ 
+			if(e.target.textContent=="+"){
+			    counter ++	 
+			}else if(e.target.textContent=="-" && counter>1){
 			    counter --
-			}else if(button==buttonIncrease[1]){
-			    counter ++
 			}
-			inputAmount.forEach((item)=>{
-				item.value =counter; 
-			})
-			
-			
+			e.currentTarget.querySelector('.amount-input').value=counter; 	
 		})
-	})*/
-let counter = 1;
+	})
 
-const buttonIncrease = document.querySelectorAll(".amount [data-amount]")
-const inputAmount = document.querySelectorAll(".amount-input");
 const cart = document.querySelectorAll('[data-cart]')	 
 const cartItem = document.querySelectorAll('[data-product-name]') 
+const desript = document.querySelectorAll('.item-description')
+const blockAmount = document.querySelectorAll('.amount')
 
 	cartItem.forEach((item)=>{
 		item.addEventListener('click', (e)=>{
 			cart.forEach((item)=>{
 				if(e.target == item){
-					let name = e.currentTarget.getAttribute('data-product-name')
-					console.log(name)
+					let nameItem = e.currentTarget.querySelector('.item-description').textContent
+					let priceItem = e.currentTarget.querySelector('.price').textContent 
+					let imgItem = e.currentTarget.querySelector('.item-image img').src
+					val = e.currentTarget.querySelector('.amount-input').value
+					console.log(nameItem)
+					console.log(priceItem)
+					console.log(imgItem)
+					console.log(val)
+					alert("Товар добавлен в корзину")
+					const amountItems = document.querySelector('.amount-items')
+					amountItems.innerHTML = val;
 			   }
-			})
-			
+			})		
 		})
 	})
+
+const buttonCart = document.querySelector(".header__cart")
+const buttonDropCart = document.querySelector(".header__drop-cart")
+ 
+ buttonCart.addEventListener('click', (e)=>{
+	 buttonDropCart.remove.style='display:none'
+	 buttonDropCart.style.cssText='display:flex'
+ })
+
+ buttonDropCart.addEventListener('click', (e)=>{
+	 buttonDropCart.remove.style='display:flex'
+	 buttonDropCart.style.cssText='display:none'
+ })
 
  
 
