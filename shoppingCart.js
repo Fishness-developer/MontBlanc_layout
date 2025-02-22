391cbf57b53218d6182d072a7016a194b50294a7
Fishness-developer <83603777+Fishness-developer@users.noreply.github.com>
2025-02-22 22:14:55 +0200

    Add files via upload

---
+++ shoppingCart.js
@@ -0,0 +1,90 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cartItemContainer = document.querySelector("[data-items]");
+    const buttonOrder = document.querySelector("[data-order-send]");
+    const cartAmountTotal = document.querySelector("[data-order-price]");
+    let productArray = [];
+    let sum = 0.00;
+    cartAmountTotal.textContent = sum.toFixed(2);
+
+    // Function update localStorage
+    const updateStorage = () => {
+        const pattern = cartItemContainer.innerHTML.trim();
+        pattern.length ? localStorage.setItem("products", pattern) : localStorage.removeItem("products");
+    };
+
+    // Function create cart from localStorage
+    const createShoppingCart = () => {
+        cartItemContainer.innerHTML = localStorage.getItem("products") || '';
+        amountTotalStorage();
+    };
+
+    // Function calculate total amount from localStorage
+    const amountTotalStorage = () => {
+        document.querySelectorAll(".cart-order-product").forEach(e => {
+            const price = Number(e.querySelector("[data-price]").textContent);
+            const value = Number(e.querySelector("[data-val]").textContent);
+            amountTotal(price * value);
+        });
+    };
+
+    // Function update total amount
+    const amountTotal = (amount) => {
+        sum += amount;
+        cartAmountTotal.textContent = sum.toFixed(2);
+    };
+
+    createShoppingCart();
+
+    cartItemContainer.addEventListener('click', (e) => {
+        if (e.target.closest('.cart-close')) {
+            const cartProduct = e.target.closest('.cart-order-product');
+            const priceClear = parseFloat(cartProduct.querySelector("[data-price]").textContent);
+            const valClear = parseInt(cartProduct.querySelector("[data-val]").textContent, 10);
+            cartProduct.remove();
+            amountTotal(-priceClear * valClear);
+            updateStorage();
+        }
+    });
+
+    // Processor for sending order
+    buttonOrder.addEventListener('click', (e) => {
+        e.preventDefault()
+        const radioGroup1 = document.querySelector('input[name="radio_1"]:checked')?.value;
+        const radioGroup2 = document.querySelector('input[name="radio_2"]:checked')?.value;
+        let personName = document.getElementById("name").value
+        let personEmail = document.getElementById("email").value
+        let personAdress = document.getElementById("adress").value
+        let personPhone = document.getElementById("phone").value
+        let personComment = document.getElementById("comment").value
+        let totalPrice = document.querySelector("[data-order-price]").textContent;
+
+        document.querySelectorAll(".cart-order-product").forEach(e => {
+            let obj = {}
+            obj.image = e.querySelector(".cart-image img").src
+            obj.name = e.querySelector("[data-name]").textContent
+            obj.price = Number(e.querySelector("[data-price]").textContent)
+            obj.value = Number(e.querySelector("[data-val]").textContent)
+            productArray.push(obj)
+        })
+
+        fetch("./order", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: personName,
+                email: personEmail,
+                adress: personAdress,
+                phone: personPhone,
+                comment: personComment,
+                delivery: radioGroup1,
+                payment: radioGroup2,
+                total: totalPrice,
+                products: productArray
+            })
+        })
+            .then(r => r.text())
+            .then(data => console.log(data))
+    })
+});
